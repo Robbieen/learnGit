2040a5196548c3e51514eeb204194d2d67cddb8f
Robbieen <Robbieen@163.com>
2018-08-14 09:56:42 +0800

    learn more orders

--- test/index.js
+++ test/index.js
@@ -1,2 +1,9 @@
-//this is my first demo! 
-// git commit -m this is a small change
+// ----------git orders---------
+// 1. git init   初始化新的管理项目
+// 2. git config --global user.name <userName> 设置用户名
+// 3. git config --global user.email <userEmail> 设置用户email
+// 4. git log  显示项目日志
+// 5. git status  显示当前目录状态
+// 6. git add <file/*> 将当前文件列表(或所有文件)保存到暂存区
+// 7. git commit -m <备注> 将暂存区文件提交到本地仓库
+
