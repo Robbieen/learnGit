898dcb8fb42bf974fd916ca1ad7558fc89fffc76
Robbieen <Robbieen@163.com>
2018-08-14 10:24:23 +0800

    add somethings

--- test/index.js
+++ test/index.js
@@ -4,6 +4,10 @@
 // 3. git config --global user.email <userEmail> 设置用户email
 // 4. git log  显示项目日志
 // 5. git status  显示当前目录状态
-// 6. git add <file/*> 将当前文件列表(或所有文件)保存到暂存区
-// 7. git commit -m <备注> 将暂存区文件提交到本地仓库
+// 6. git add <file> 将当前文件列表保存到暂存区
+// 7. git add <*>  将多有文件列表添加到暂存区
+// 8. git commit -m <备注>  将暂存区文件提交到本地仓库
+// 9. git checkout <file>  回滚文件修改的内容
+// 10. git checkout .  回滚所有的修改
+// 11. git rm --cached <file>  add的逆操作
 
